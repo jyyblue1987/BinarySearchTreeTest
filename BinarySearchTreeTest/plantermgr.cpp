63fefbbbc5cc2f76ae0b232a4563e6f02189c8b7
jyyblue <jyyblue1987@outlook.com>
2021-03-13 23:51:34 +0800

    [add] add/remove function.

--- BinarySearchTreeTest/plantermgr.cpp
+++ BinarySearchTreeTest/plantermgr.cpp
@@ -1,21 +1,131 @@
 #include "plantermgr.h"
+#include <iostream>
+#include <string.h>
 
+#define NULL 0
+
+using namespace std;
 
 plantermgr::plantermgr(void)
 {
+	root = NULL;
+	size = 0;
 }
+
+plantermgr::~plantermgr(void) 
+{
+	clear();
+}
+
+bool plantermgr::clear() {
+	freeNodeTree(root);
+	root = NULL;
+	size = 0;
+
+	return true;
+}
+
+void plantermgr::freeNodeTree(planter* node) {
+	if (node != NULL)
+	{
+		freeNodeTree(node->left);
+		freeNodeTree(node->right);
+
+		delete node->name;
+		delete node;
+	}
+}
+
+planter * plantermgr::insert(planter * root, planter * newNode) {
+	if (root == NULL)
+		return newNode;
+	if (newNode == NULL)
+		return root;
+
+	int key = newNode->num;
+	if (key < root->num)
+		root->left = insert(root->left, newNode);
+	else if (key > root->num)
+		root->right = insert(root->right, newNode);
+
+	return root;
+}
+
 
 void plantermgr::plant(int num, char *name) 
 {
+	planter *node = new planter;
+	node->left = NULL;
+	node->right = NULL;
+	node->num = num;
+	node->name = new char[strlen(name) + 1];
+	strcpy(node->name, name);
 
+	root = insert(root, node);
 }
 
-void plantermgr::harvest(char *name, int start, int stop)
+void plantermgr::harvest(char *name, int start, int end)
 {
+	if (root == NULL)
+		return;
 
+	search(root, name, start, end);
 }
+
+void plantermgr::search(planter *root, char *name, int start, int end)
+{
+	if( start > end )
+		return;
+
+	if( root == NULL )
+		return;
+
+	if( strcmp(name, root->name) == 0 && start <= root->num && root->num <= end )
+		cout << root->num << endl;
+	
+	search(root->left, name, start, root->num);
+	search(root->right, name, root->num, end);
+}
+
 
 void plantermgr::prune(int num)
 {
+	if (root == NULL)
+		return;
+
+	planter *curNode = root;
+	planter* prevNode = root;
+
+	while (curNode != NULL) {
+		if (curNode->num == num)
+			break;
+
+		prevNode = curNode;
+		if (curNode->num < num)
+			curNode = curNode->right;
+		else if (curNode->num > num)
+			curNode = curNode->left;		
+	}
+
+	if (curNode == NULL)
+		return;
+
+	if (curNode == root) {
+		root = curNode->left;
+		root = insert(root, curNode->right);
+	}
+	else {
+		if (curNode->num < prevNode->num)
+			prevNode->left = NULL;
+		else
+			prevNode->right = NULL;
+		root = insert(root, curNode->left);
+		root = insert(root, curNode->right);
+	}
+
+	delete curNode->name;
+	delete curNode;
+
+	size--;
 
 }
--- BinarySearchTreeTest/plantermgr.h
+++ BinarySearchTreeTest/plantermgr.h
@@ -1,8 +1,26 @@
 #pragma once
+struct planter {
+	int num;
+	char *name;
+
+	planter *left;
+	planter *right;
+};
+
 class plantermgr
 {
+private:
+	planter* root;		// root to the left
+	int size;
+
+	bool clear();
+	planter* insert(planter *root, planter *newNode);
+	void freeNodeTree(planter* node);
+	void search(planter *root, char *name, int start, int end);
+
 public:
 	plantermgr(void);
+	~plantermgr();
 
 	void plant(int num, char *name);
 	void harvest(char *name, int start, int stop);
